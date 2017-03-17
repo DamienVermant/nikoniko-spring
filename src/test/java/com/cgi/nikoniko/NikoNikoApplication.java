ace4241c9adf1c97b8163ccb48ee846e28012686
Vermant Damien <damien.vermant@gmail.com>
2017-03-17 10:41:26 +0100

    initial commit

---
+++ src/test/java/com/cgi/nikoniko/NikoNikoApplication.java
@@ -0,0 +1,12 @@
+package com.cgi.nikoniko;
+
+import org.springframework.boot.SpringApplication;
+
+import com.cgi.nikoniko.NikoNikoApplication;
+
+public class NikoNikoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NikoNikoApplication.class, args);
+	}
+}
